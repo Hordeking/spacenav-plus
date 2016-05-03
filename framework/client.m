aa4f91d9d62fb882d7cf101f4cc91f7ad24bd145
Thomas Buck <xythobuz@xythobuz.de>
2016-05-03 15:52:33 +0200

    Implemented axis handling 3DconnexionClient spacenavd adapter.

--- framework/client.m
+++ framework/client.m
@@ -1,9 +1,11 @@
 #import "ConnexionClientAPI.h"
+#include <mach/mach_time.h>
 #include <spnav.h>
+#include <pthread.h>
 
-#define DEBUG
-#define DEBUG_CALLS
-#define ERROR_OUTPUT
+//#define DEBUG
+//#define DEBUG_CALLS
+//#define ERROR_OUTPUT
 
 #if defined(DEBUG) || defined(DEBUG_CALLS) || defined(ERROR_OUTPUT)
 #include <stdio.h>
@@ -30,15 +32,88 @@ static ConnexionAddedHandlerProc theAddedHandler = NULL;
 // void removedHandler(io_connect_t con);
 static ConnexionRemovedHandlerProc theRemovedHandler = NULL;
 
+static ConnexionDeviceState device;
 static int spnav_opened = 0;
 
 static Clients clients[MAX_CLIENTS];
 static int nextUnusedClient = 0;
 
+static pthread_t thread;
+static int threadStarted = 0;
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+static void handleEvent(spnav_event *event) {
+    if (event->type == SPNAV_EVENT_MOTION) {
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            if (clients[i].registered != 0) {
+                device.client = i;
+                device.command = kConnexionCmdHandleAxis;
+                device.time = mach_absolute_time();
+                device.buttons8 = 0;
+                device.buttons = 0;
+                device.axis[0] = -event->motion.x;
+                device.axis[1] = -event->motion.y;
+                device.axis[2] = -event->motion.z;
+                device.axis[3] = -event->motion.rx;
+                device.axis[4] = -event->motion.ry;
+                device.axis[5] = -event->motion.rz;
+
+                theMessageHandler(0, kConnexionMsgDeviceState, &device);
+            }
+        }
+    } else if (event->type == SPNAV_EVENT_BUTTON) {
+        // We're not yet handling button events
+    } else {
+#ifdef ERROR_OUTPUT
+        fprintf(stderr, "Invalid spacenavd event type: %d\n", event->type);
+#endif
+    }
+}
+
+static void *spacenav_thread(void *argument) {
+    spnav_event event;
+
+    for (;;) {
+        int result = spnav_wait_event(&event);
+        if (result != 0) {
+            pthread_mutex_lock(&mutex);
+            if (theMessageHandler != NULL) {
+                handleEvent(&event);
+            }
+            pthread_mutex_unlock(&mutex);
+        } else {
+#ifdef ERROR_OUTPUT
+            fprintf(stderr, "Error waiting for spacenavd data!\n");
+#endif
+            pthread_mutex_lock(&mutex);
+            threadStarted = 0;
+            pthread_mutex_unlock(&mutex);
+            return NULL;
+        }
+    }
+
+    return NULL;
+}
+
 OSErr InstallConnexionHandlers(ConnexionMessageHandlerProc messageHandler, ConnexionAddedHandlerProc addedHandler, ConnexionRemovedHandlerProc removedHandler) {
 #ifdef DEBUG_CALLS
     printf("InstallConnexionHandlers(%p, %p, %p);\n", messageHandler, addedHandler, removedHandler);
 #endif
+
+    pthread_mutex_lock(&mutex);
+
+    device.version = kConnexionDeviceStateVers;
+    device.param = 0;
+    device.value = 0;
+    device.address = 0;
+    device.report[0] = 0;
+    device.report[1] = 0;
+    device.report[2] = 0;
+    device.report[3] = 0;
+    device.report[4] = 0;
+    device.report[5] = 0;
+    device.report[6] = 0;
+    device.report[7] = 0;
 
     nextUnusedClient = 0;
     for (int i = 0; i < MAX_CLIENTS; i++) {
@@ -50,28 +125,50 @@ OSErr InstallConnexionHandlers(ConnexionMessageHandlerProc messageHandler, Conne
     theRemovedHandler = removedHandler;
 
     if (messageHandler != NULL) {
-        if (spnav_opened != 0) {
+        if (spnav_opened == 0) {
+#ifdef DEBUG
+            printf("opening spacenavd connection...\n");
+#endif
+            int result = spnav_open();
+            if (result != -1) {
+                spnav_opened = 1;
+            } else {
+#ifdef ERROR_OUTPUT
+                fprintf(stderr, "Error opening spacenavd connection!\n");
+#endif
+                pthread_mutex_unlock(&mutex);
+                return -1; // could not open spnavd
+            }
+        } else {
 #ifdef DEBUG
             printf("spacenavd connection is already open!\n");
 #endif
-            return 0;
         }
 
+        if (threadStarted == 0) {
 #ifdef DEBUG
-        printf("opening spacenavd connection...\n");
+            printf("starting spacenavd thread...\n");
 #endif
-        int result = spnav_open();
-        if (result != -1) {
-            spnav_opened = 1;
-            return 0;
-        } else {
+            if (pthread_create(&thread, NULL, spacenav_thread, NULL) != 0) {
 #ifdef ERROR_OUTPUT
-            fprintf(stderr, "Error opening spacenavd connection!\n");
+                fprintf(stderr, "Error starting spacenavd thread!\n");
 #endif
-            return -1; // could not open spnavd
+                pthread_mutex_unlock(&mutex);
+                CleanupConnexionHandlers();
+                return -1; // could not start thread
+            } else {
+                threadStarted = 1;
+                pthread_mutex_unlock(&mutex);
+                return 0;
+            }
+        } else {
+#ifdef DEBUG
+            printf("spacenavd thread already started...\n");
+#endif
         }
     }
 
+    pthread_mutex_unlock(&mutex);
     return -1; // did not pass a valid messageHandler
 }
 
@@ -79,6 +176,8 @@ void CleanupConnexionHandlers(void) {
 #ifdef DEBUG_CALLS
     printf("CleanupConnexionHandlers();\n");
 #endif
+
+    pthread_mutex_lock(&mutex);
 
     if (spnav_opened != 0) {
 #ifdef DEBUG
@@ -88,20 +187,38 @@ void CleanupConnexionHandlers(void) {
 #ifdef ERROR_OUTPUT
             fprintf(stderr, "Error closing spacenavd connection!\n");
 #endif
+        } else {
+            spnav_opened = 0;
         }
-        spnav_opened = 0;
+    }
+
+    if (threadStarted != 0) {
+#ifdef DEBUG
+        printf("canceling spacenavd thread...\n");
+#endif
+        if (pthread_cancel(thread) != 0) {
+#ifdef ERROR_OUTPUT
+            fprintf(stderr, "Error canceling spacenavd thread!\n");
+#endif
+        } else {
+            threadStarted = 0;
+        }
     }
 
     nextUnusedClient = 0;
     for (int i = 0; i < MAX_CLIENTS; i++) {
         clients[i].registered = 0;
     }
+
+    pthread_mutex_unlock(&mutex);
 }
 
 UInt16 RegisterConnexionClient(UInt32 signature, UInt8 *name, UInt16 mode, UInt32 mask) {
 #ifdef DEBUG_CALLS
     printf("RegisterConnexionClient(%d, \"%s\", %d, %d);\n", (unsigned int)signature, name, mode, (unsigned int)mask);
 #endif
+
+    pthread_mutex_lock(&mutex);
 
     int clientID = nextUnusedClient;
     if (clientID >= MAX_CLIENTS) {
@@ -124,6 +241,8 @@ UInt16 RegisterConnexionClient(UInt32 signature, UInt8 *name, UInt16 mode, UInt3
     clients[clientID].mask = mask;
     clients[clientID].buttonMask = 0;
 
+    pthread_mutex_unlock(&mutex);
+
     return clientID;
 }
 
@@ -132,6 +251,8 @@ void SetConnexionClientMask(UInt16 clientID, UInt32 mask) {
     printf("SetConnexionClientMask(%d, %d);\n", clientID, (unsigned int)mask);
 #endif
 
+    pthread_mutex_lock(&mutex);
+
     if (clientID < MAX_CLIENTS) {
         if (clients[clientID].registered == 0) {
 #ifdef ERROR_OUTPUT
@@ -142,10 +263,12 @@ void SetConnexionClientMask(UInt16 clientID, UInt32 mask) {
 #ifdef ERROR_OUTPUT
         fprintf(stderr, "Trying to modify invalid spacenavd client ID: %d\n", clientID);
 #endif
+        pthread_mutex_unlock(&mutex);
         return;
     }
 
     clients[clientID].mask = mask;
+    pthread_mutex_unlock(&mutex);
 }
 
 void SetConnexionClientButtonMask(UInt16 clientID, UInt32 buttonMask) {
@@ -153,6 +276,8 @@ void SetConnexionClientButtonMask(UInt16 clientID, UInt32 buttonMask) {
     printf("SetConnexionClientButtonMask(%d, %d);\n", clientID, (unsigned int)buttonMask);
 #endif
 
+    pthread_mutex_lock(&mutex);
+
     if (clientID < MAX_CLIENTS) {
         if (clients[clientID].registered == 0) {
 #ifdef ERROR_OUTPUT
@@ -163,16 +288,20 @@ void SetConnexionClientButtonMask(UInt16 clientID, UInt32 buttonMask) {
 #ifdef ERROR_OUTPUT
         fprintf(stderr, "Trying to modify invalid spacenavd client ID: %d\n", clientID);
 #endif
+        pthread_mutex_unlock(&mutex);
         return;
     }
 
     clients[clientID].buttonMask = buttonMask;
+    pthread_mutex_unlock(&mutex);
 }
 
 void UnregisterConnexionClient(UInt16 clientID) {
 #ifdef DEBUG_CALLS
     printf("UnregisterConnexionClient(%d);\n", clientID);
 #endif
+
+    pthread_mutex_lock(&mutex);
 
     if (clientID < MAX_CLIENTS) {
         if (clients[clientID].registered == 0) {
@@ -190,6 +319,8 @@ void UnregisterConnexionClient(UInt16 clientID) {
         fprintf(stderr, "Trying to modify invalid spacenavd client ID: %d\n", clientID);
 #endif
     }
+
+    pthread_mutex_unlock(&mutex);
 }
 
 OSErr ConnexionControl(UInt32 message, SInt32 param, SInt32 *result) {
